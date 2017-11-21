7563b7ed44f49dfffb5e208c21d34ea048b23379
Jason Vance <jbvance@gmail.com>
2017-11-21 19:32:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,14 @@ def oxford_comma(array)
     return array[0]
   elsif array.size == 2
     return "#{array[0]} and #{array[1]}"
+  elsif array.size >= 3
+    tmp = ""
+    array.each_with_index do |el, index|
+      if index == array.size - 1
+        tmp += " and #{el}"
+      else 
+        tmp += el + ", "
+      end
+    end
   end
 end
